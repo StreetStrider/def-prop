4829db67780a0c2c31e7532dcf441c18c27c9cb5
StreetStrider <teamfortresslife@gmail.com>
2016-05-31 23:19:05 +0300

    more tests and coverage.

---
+++ test/case/lib/extract.test.js
@@ -0,0 +1,15 @@
+
+var expect = require('chai').expect
+
+describe('extract', () =>
+{
+	var extract = require('../../../lib/extract')
+
+	it('works', () =>
+	{
+		expect(extract()).deep.equal({})
+		expect(extract([ ':enum', ':write' ])).deep.equal({ enumerable: true, writable: true })
+		expect(extract([ ':config' ])).deep.equal({ configurable: true })
+		expect(extract([ ':enum', ':write' ], 1)).deep.equal({ writable: true })
+	})
+})
