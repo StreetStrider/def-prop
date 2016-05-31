69230a2d373d7652d7cf9b3fe4d8c7390d6e0b75
StreetStrider <teamfortresslife@gmail.com>
2016-06-01 00:11:44 +0300

    getset.

--- getset.js
+++ getset.js
@@ -1,5 +1,16 @@
 
+var extract = require('./lib/extract')
+
 module.exports = function getset (getter, setter)
 {
-	
+	var descr = extract(arguments, 1)
+
+	descr.get = getter
+
+	if (typeof setter === 'function')
+	{
+		descr.set = setter
+	}
+
+	return descr
 }
